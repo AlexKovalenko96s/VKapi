380ebd0c104b5ca229b7b415b346a1084311f9aa
Alex Kovalenko <alex.kovalenko.s96@gmail.com>
2017-01-06 20:52:10 +0200

    //

--- VKtestV3/src/ua/kas/vkTest/Main.java
+++ VKtestV3/src/ua/kas/vkTest/Main.java
@@ -7,13 +7,19 @@ import java.net.URISyntaxException;
 import java.net.URL;
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.apache.http.client.ClientProtocolException;
 
 public class Main {
 
 	private static ArrayList<String> list = new ArrayList<>();
-	private static String access_token = "ed3ffdef7c885ef6863a50259f8fdfc93f6920bc5e3fc2bb548dceb5036637e586455d3dd7d24458e1871";
+	private static ArrayList<String> listIdWall = new ArrayList<>();
+	private static Map countLike = new HashMap<String, Integer>();
+
+	// http://oauth.vk.com/authorize?client_id=5809983&scope=messages,wall,friends,likes,users&redirect_uri=http://oauth.vk.com/blank.html&display=popup&response_type=token
+	private static String access_token = "70b6fb754c2b39085ae447c80b6e2cea3aea304c5551e9ff7dbfe1c3ff77015e020b378f6502a5597dad2";
 
 	public static String getFriendsList() throws IOException {
 		String url = "https://api.vk.com/method/" + "friends.get" + "?user_id=224429310" + "&return_system=1"
@@ -42,12 +48,11 @@ public class Main {
 	}
 
 	public static String sendMessage() throws IOException {
-		String space = "%20";
-
+		// String space = "%20";
 		// %0A
-		// String enter = ""
-		String url = "https://api.vk.com/method/" + "messages.send" + "?user_id=110602479" + "&title=title"
-				+ "&message=віпвіі%20ds%0Anпа" + "&access_token=" + access_token;
+
+		String url = "https://api.vk.com/method/" + "messages.send" + "?user_id=202930417" + "&title=title"
+				+ "&message=привет" + "&access_token=" + access_token;
 		String line = "";
 		URL url2 = new URL(url);
 		BufferedReader reader = new BufferedReader(new InputStreamReader(url2.openStream()));
@@ -69,28 +74,85 @@ public class Main {
 		return line;
 	}
 
-	public static void main(String[] args) throws ClientProtocolException, NoSuchAlgorithmException, IOException,
-			URISyntaxException, InterruptedException {
+	public static void likes() throws IOException {
+		String url = "https://api.vk.com/method/" + "likes.getList" + "?type=post" + "&owner_id=34582594"
+				+ "&item_id=3966" + "&access_token=" + access_token;
+		String line = "";
+		URL url2 = new URL(url);
+		BufferedReader reader = new BufferedReader(new InputStreamReader(url2.openStream()));
+		line = reader.readLine();
+		reader.close();
 
-		String idFriends = getFriendsList();
-
+		System.out.println(line);
 		for (int i = 0;; i++) {
 			if (i == 0) {
-				list.add(idFriends.substring(idFriends.indexOf("[") + 1, idFriends.indexOf(",")));
-				idFriends = idFriends.substring(idFriends.indexOf(",") + 1);
+				line = line.substring(line.indexOf(",") + 1);
+				list.add(line.substring(line.indexOf("[") + 1, line.indexOf(",")));
+				line = line.substring(line.indexOf(",") + 1);
 			} else {
 				try {
-					list.add(idFriends.substring(0, idFriends.indexOf(",")));
-					idFriends = idFriends.substring(idFriends.indexOf(",") + 1);
+					list.add(line.substring(0, line.indexOf(",")));
+					line = line.substring(line.indexOf(",") + 1);
 				} catch (Exception ex) {
-					list.add(idFriends.substring(0, idFriends.indexOf("]")));
+					list.add(line.substring(0, line.indexOf("]")));
 					break;
 				}
 			}
 		}
-		// System.out.println(list);
-		// getUserName();
-		System.out.println(sendMessage());
+		System.out.println(list);
+	}
+
+	public static void wall() throws IOException {
+		String url = "https://api.vk.com/method/" + "wall.get" + "?owner_id=34582594" + "&filter=owner" + "&count=100"
+				+ "&access_token=" + access_token;
+		String line = "";
+		URL url2 = new URL(url);
+		BufferedReader reader = new BufferedReader(new InputStreamReader(url2.openStream()));
+		line = reader.readLine();
+		reader.close();
+		System.out.println(line);
+
+		for (int i = 0; 100 > i; i++) {
+			listIdWall.add(line.substring(line.indexOf("id:") + 5, line.indexOf(",")));
+			line = line.substring(line.indexOf("\"id\":") + 1);
+		}
+		listIdWall.remove(0);
+	}
+
+	public static void countWallLikes() throws InterruptedException {
+		for (int i = 0; i < listIdWall.size(); i++) {
+			Thread.sleep(250);
+		}
+	}
+
+	public static void main(String[] args) throws ClientProtocolException, NoSuchAlgorithmException, IOException,
+			URISyntaxException, InterruptedException {
+
+		// String idFriends = getFriendsList();
+		// String idFriends = likes();
+		//
+		// for (int i = 0;; i++) {
+		// if (i == 0) {
+		// list.add(idFriends.substring(idFriends.indexOf("[") + 1,
+		// idFriends.indexOf(",")));
+		// idFriends = idFriends.substring(idFriends.indexOf(",") + 1);
+		// } else {
+		// try {
+		// list.add(idFriends.substring(0, idFriends.indexOf(",")));
+		// idFriends = idFriends.substring(idFriends.indexOf(",") + 1);
+		// } catch (Exception ex) {
+		// list.add(idFriends.substring(0, idFriends.indexOf("]")));
+		// break;
+		// }
+		// }
+		// }
+
+		// System.out.println(sendMessage());
 		// System.out.println(wallPost());
+
+		likes();
+		// getUserName();
+
+		// wall();
 	}
 }
